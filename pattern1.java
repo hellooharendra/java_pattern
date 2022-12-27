85e518d786d8919d9796b5a51e2039e740934de0
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-12-28 00:51:09 +0530

    Add files via upload

--- pattern1.java
+++ pattern1.java
@@ -1,12 +1,11 @@
-public class pattern1{
+public class pattern1 {
     public static void main(String[] args) {
-        int i,j;
-        for(i=0;i<=5;i++){
-           for(j=0;j<=i;j++)
-           {
+        for(int i=0;i<=5;i++){
+           for(int j=0;j<=i;j++){
             System.out.print("*");
-           }
-           System.out.println();
+           }System.out.println("");
         }
+        
     }
+    
 }
